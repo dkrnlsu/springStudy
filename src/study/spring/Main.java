234304bb3c8506fed2c2b2b8ae92909c996755f1
jihyun.oh <jihyun.oh@sk.com>
2013-04-23 12:42:39 +0900

    first commit

---
+++ src/study/spring/Main.java
@@ -0,0 +1,22 @@
+package study.spring;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ojh
+ * Date: 13. 4. 22
+ * Time: 오후 6:29
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Client client = new Client();
+        Log defaultLog = new DefaultLog();
+        Log prettyLog = new PrettyLog();
+
+        client.setLog(defaultLog);
+        client.biz("Print DefaultLog");
+
+        client.setLog(prettyLog);
+        client.biz("Print PrettyLog");
+    }
+}
