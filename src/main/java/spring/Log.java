0c1349041ecfb3b97a49b06ce245b9a004411bb7
jihyunoh <jihyun.oh@sk.com>
2013-05-02 15:39:23 +0900

    maven으로 변경

---
+++ src/main/java/spring/Log.java
@@ -0,0 +1,12 @@
+package spring;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ojh
+ * Date: 13. 4. 22
+ * Time: 오후 6:38
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Log {
+    void write(String data);
+}
---
+++ src/main/java/spring/PrettyLog.java
@@ -0,0 +1,17 @@
+package spring;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ojh
+ * Date: 13. 4. 22
+ * Time: 오후 6:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class PrettyLog implements Log {
+    @Override
+    public void write(String data) {
+        System.out.println("**************************************");
+        System.out.println("data : " + data);
+        System.out.println("**************************************");
+    }
+}
